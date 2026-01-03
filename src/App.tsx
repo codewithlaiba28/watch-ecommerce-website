66f900654ee3d019de5d21e26a38a470192cea3d
Laiba Khan <codewithlaiba28@gmail.com>
2026-01-03 22:58:38 +0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -8,27 +8,29 @@ import Women from './Pages/Women.tsx'
 import Store from './Pages/Store.tsx'
 import Smart from './Pages/Smart.tsx'
 import Offers from './Pages/Offers.tsx'
-import Brands from './Pages/Brands.tsx'
+import Brands from '../Brands.tsx'
 import Contact from './Pages/Contact.tsx'
 import Cart from './Pages/Cart.tsx'
+import Search from "./Pages/Search.tsx"
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Navbar/>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/internationalbrands' element={<Internationalbrands />} />
-      <Route path='/allwatches' element={<Allwatches />} />
-      <Route path='/men' element={<Men />} />
-      <Route path='/women' element={<Women />} />
-      <Route path='/store' element={<Store />} />
-      <Route path='/smart' element={<Smart />} />
-      <Route path='/offers' element={<Offers />} />
-      <Route path='/brands' element={<Brands />} />
-      <Route path='/contact' element={<Contact />} />
-      <Route path='/cart' element={<Cart />} />
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/internationalbrands' element={<Internationalbrands />} />
+        <Route path='/allwatches' element={<Allwatches />} />
+        <Route path='/men' element={<Men />} />
+        <Route path='/women' element={<Women />} />
+        <Route path='/store' element={<Store />} />
+        <Route path='/smart' element={<Smart />} />
+        <Route path='/offers' element={<Offers />} />
+        <Route path='/brands' element={<Brands />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/search' element={<Search />} />
+      </Routes>
     </BrowserRouter>
   )
 }
